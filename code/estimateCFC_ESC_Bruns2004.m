b6dc0c5164737cf46fc8ec1d3d097b9bcc5e8342
Il Memming Park <memming@gmail.com>
2015-08-31 15:25:56 -0400

    Missing absolute value of the correlation
    Only magnitude matters, not the sign!

--- code/estimateCFC_ESC_Bruns2004.m
+++ code/estimateCFC_ESC_Bruns2004.m
@@ -4,8 +4,10 @@ function CFC = estimateCFC_ESC_Bruns2004(fLow, fHigh, xLow, xHigh, aLow, phiLow,
 % Weakness: can't detect if coupling is at double the frequency
 % Weakness: can't detect if amplitude has symmetric deviation (for example
 %           increased variance)
+% Weakness: can't discriminate between common amplidue gain and
+%           phase-correlation
 
 CFC = zeros(size(xLow, 2), 1);
 for k = 1:size(xLow, 2)
-    CFC(k) = corr(xLow(:,k), aHigh(:,k));
+    CFC(k) = abs(corr(xLow(:,k), aHigh(:,k)));
 end
