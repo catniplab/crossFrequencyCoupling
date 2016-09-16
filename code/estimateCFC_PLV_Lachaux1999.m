84f3064bcf5474be7a907ae561c4d064fa221346
Il Memming Park <memming@gmail.com>
2016-09-16 15:23:19 -0400

    BUG FIX! Phase of the amplitude of the High
    it was using the phase of the high which is incorrect
    
    Need double Hilbert transform

--- code/estimateCFC_PLV_Lachaux1999.m
+++ code/estimateCFC_PLV_Lachaux1999.m
@@ -1,9 +1,14 @@
 function CFC = estimateCFC_PLV_Lachaux1999(fLow, fHigh, xLow, xHigh, aLow, phiLow, aHigh, phiHigh)
-% Phase-locking value (PLV)
+% Phase-locking value (PLV) 
+%    phase of the amplitude of the high frequency should be locked 
+%    with the phase of the low frequency signal.
 %
 % Weakness: can't detect if coupling is at integer multiples of the lower frequency
 % Weakness: when amplitude is small, and phase slip happens, you know.
 
+% We need a second Hilbert transform to do it.
+phiAmpHigh = angle(hilbert(aHigh));
+
 sz = size(xLow);
-CFC = abs(mean(exp(1i*(phiLow - phiHigh))));
+CFC = abs(mean(exp(1i*(phiLow - phiAmpHigh))));
 CFC = permute(CFC, [2:numel(sz), 1]);
